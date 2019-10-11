205318544e2808c35f592513f32e790376720453
Javier Porlono <pauli44ef@gmail.com>
2019-10-11 19:57:23 +0200

    rendering styling of exams

--- public/comunication.js
+++ public/comunication.js
@@ -1,39 +1,51 @@
 // Create DOM elements:
 const examenesContainer = document.getElementById('root');
+// Display oculto para que no sea el estilo de overflow:scroll
+examenesContainer.style.display = 'none';
+
 const examContainer = document.createElement('div')
 
-examContainer.setAttribute('class', 'exam-container');
-
+examContainer.setAttribute('class', 'container');
 examenesContainer.appendChild(examContainer)
 
 // Request and rendering:
 var request = new XMLHttpRequest()
-
 request.open('GET', 'http://localhost:4000/api/examenes', true)
 
-request.onload = function(){
-    var examenes = JSON.parse(this.response)
+// Fetch exams and render:
+function getExamenes(){
 
-    if(request.status >= 200 && request.status < 400){
+    // Activar display del container que habiamos ocultado:
+    examenesContainer.style.display = 'block';
 
-        examenes.forEach(examen => {
+    request.onload = function(){
+        var examenes = JSON.parse(this.response)
+    
+        if(request.status >= 200 && request.status < 400){
+    
+            examenes.forEach(examen => {
+                
+                const examenWrapper = document.createElement('div')
+                examenWrapper.setAttribute('class', 'exam-container')
+    
+                const titulo = document.createElement('h4')
+                titulo.textContent = examen.titulo;
+                examenWrapper.appendChild(titulo);
+    
+                const dificultad = document.createElement('h5')
+                dificultad.textContent = "Dificultad estimada: " + examen.dificultad_info;
+                examenWrapper.appendChild(dificultad);
+    
+                const numeroPreguntas = document.createElement('p')
+                numeroPreguntas.textContent = "Numero de Preguntas: " + examen.numero_preguntas;
+                examenWrapper.appendChild(numeroPreguntas);
+    
+                examContainer.appendChild(examenWrapper);
+            })
+        } else { console.log(error) }
+    }
+    
+    request.send()
 
-            const titulo = document.createElement('h2')
-            const dificultad = document.createElement('h5')
-            const numeroPreguntas = document.createElement('p')
-
-            titulo.textContent = examen.titulo;
-            dificultad.textContent = examen.dificultad_info;
-            numeroPreguntas.textContent = examen.numero_preguntas;
-
-            examenesContainer.appendChild(titulo);
-            examenesContainer.appendChild(dificultad);
-            examenesContainer.appendChild(numeroPreguntas);
-
-            examenesContainer.appendChild(examContainer);
-        })
-    } else { console.log(error) }
 }
-
-request.send()
 
